f3850582cd8d69827a125e7a6fae5d3fcaf4688e
adibenedetto117 <75105824+adibenedetto117@users.noreply.github.com>
2022-04-20 19:33:53 -0500

    Creation of Project

---
+++ ProteinCalculator/ProteinCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinCalculatorApp.swift
+//  ProteinCalculator
+//
+//  Created by Anthony Dibenedetto on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ProteinCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
